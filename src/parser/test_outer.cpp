fbdeb466f5a3396b274d00cd5159c425b421eee8
Matthew Sherborne <msherborne@gmail.com>
2014-09-05 10:59:50 +1000

    Now detects COLON

--- src/parser/test_outer.cpp
+++ src/parser/test_outer.cpp
@@ -25,26 +25,46 @@ go_bandit([]() {
       std::string json{"   ,xxx   "};
       Status status(json.cbegin(), json.cend());
       Token result = getNextOuterToken(status);
+      
       AssertThat(result, Equals(COMMA));
+
     });
 
-    it("3: Can read COLON", [&]() { assert(false); });
+    it("3: Can read COLON", [&]() { 
+      std::string json{"   :   "};
+      Status status(json.cbegin(), json.cend());
+      Token result = getNextOuterToken(status);
+      AssertThat(result, Equals(COLON));
+    });
+
     it("4: Can read null", [&]() {
-      assert(false);
+      AssertThat(false, Equals(true));
     });
     it("5: Can read boolean", [&]() {
-      assert(false);
+      AssertThat(false, Equals(true));
     });
     it("6: Can read array", [&]() {
-      assert(false);
+      AssertThat(false, Equals(true));
     });
-    it("7: Can read ARRAY_END", [&]() { assert(false); });
-    it("8: Can read object", [&]() { assert(false); });
+    it("7: Can read ARRAY_END", [&]() { 
+      AssertThat(false, Equals(true));
+    });
+    it("8: Can read object", [&]() { 
+      AssertThat(false, Equals(true));
+    });
 
-    it("9: Can read OBJECT_END", [&]() { assert(false); });
-    it("10: Can read number", [&]() { assert(false); });
-    it("11: Can read string", [&]() { assert(false); });
-    it("12: Can read ERROR", [&]() { assert(false); });
+    it("9: Can read OBJECT_END", [&]() { 
+      AssertThat(false, Equals(true));
+    });
+    it("10: Can read number", [&]() { 
+      AssertThat(false, Equals(true));
+    });
+    it("11: Can read string", [&]() { 
+      AssertThat(false, Equals(true));
+    });
+    it("12: Can read ERROR", [&]() { 
+      AssertThat(false, Equals(true));
+    });
   });
 });
 
