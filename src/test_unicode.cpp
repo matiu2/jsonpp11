eee191323a4c436a9cd648c05e8bbdfba984ac22
Matthew Sherborne <msherborne@gmail.com>
2014-09-21 00:08:51 +1000

    UTF-8 Can read a 5 byte encoded char

--- src/test_unicode.cpp
+++ src/test_unicode.cpp
@@ -64,6 +64,14 @@ go_bandit([]() {
       AssertThat(output, Equals(expected));
     });
 
+    it("2.5 Can read a 5 byte encoded char", [&]() {
+      std::string input = u8"�";
+      std::u32string output = U"";
+      std::u32string expected = U"�";
+      from8(input.cbegin(), std::back_inserter(output));
+      AssertThat(output, Equals(expected));
+    });
+
   });
 });
 
